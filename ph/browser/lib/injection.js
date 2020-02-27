08c0c9ad8534bdadf380b5d6af9f6f5d8a8fcaa6
magbone <2300427529@qq.com>
2020-02-27 10:49:03 +0800

    update to 1.0.1

--- ph/browser/lib/injection.js
+++ ph/browser/lib/injection.js
@@ -1,14 +1,31 @@
 function injectCustomJS(jsPath)
 {
-      console.log('Injection js');
       jsPath = jsPath || 'lib/pd.js';
       var temp = document.createElement('script');
       temp.setAttribute('type', 'text/javascript');
-      console.log(chrome.extension.getURL(jsPath))
       temp.src = chrome.extension.getURL(jsPath);
       document.head.appendChild(temp);
 }
 
 injectCustomJS();
 
+
+window.addEventListener("message", function(event){
+      if (event.source != window)
+            return;
+      
+      if (event.data.type && (event.data.type == "FROM_PAGE")){
+            let data = event.data;
+            if (data.id === 'nuh43njwjer0234c'){
+                  /**
+                   * Passing the message to the popup per 0.6 sec 
+                   */
+                  window.setInterval(
+                        chrome.runtime.sendMessage, 600, {data: data.value}, function(response){
+                              // Noting to do
+                        }
+                  );
+            }
+      }
+}, false);
 
--- ph/browser/lib/pd.js
+++ ph/browser/lib/pd.js
@@ -6,126 +6,18 @@
 
 
 (function(){
-
-
       MG_Utils.domReady(function(){
             console.debug('dom ready');
-            
-            createElement(document.head, 
-                  'link', 
-                  [{
-                        key: 'href', 
-                        value:'//cdnjs.cloudflare.com/ajax/libs/mdui/0.4.3/css/mdui.min.css'
-            },{
-                  key: 'rel',
-                  value: 'stylesheet'
-            }]);
-            createElement(document.head,
-                  'script',
-                  [{
-                        key: 'src',
-                        value: '//cdnjs.cloudflare.com/ajax/libs/mdui/0.4.3/js/mdui.min.js'
-            }])
-
-            createElement(document.body,
-                  'div',
-                  [{
-                        key: 'id',
-                        value: 'pornhub-video-download-xxx',
-                  },{
-                        key: 'style',
-                        value: 'position: relative'
-                  }])
-            
-            createElement(document.body,
-                  'div',
-                  [{
-                        key: 'id',
-                        value: 'download-dialog-container'
-                  }])
-            createElement(document.body,
-                  'input',
-                  [{
-                        key: 'id',
-                        value: 'donwload-link-input'
-                  },{
-                        key: 'style',
-                        value: 'display: none'
-                  }])
-            const floatButton = '<button class="mdui-fab mdui-fab-fixed mdui-color-pink-accent mdui-ripple" style="z-index:999" id="download-dialog-open"> \
-            <i class="mdui-icon material-icons">&#xe2c4;</i></button>';
-            var fb = document.getElementById('pornhub-video-download-xxx');
-            fb && (fb.innerHTML = floatButton);
-
-            var dd = document.getElementById('download-dialog-container');
-
-            
-            var trs = '';
             for(var attr in window){
                   if (attr.indexOf('qualityItems') !== -1){
-                        var values = window[attr];
-                        
-                        values.forEach((v, i)=>{
-                              var id = 'll-select' + i;
-                              const link = '<input  class="mdui-textfield-input" type="text" readonly id="' + id +  '" style="border:none" value="' + v['url'] +'"/>'
-                              trs += '<tr>\
-                                    <td>' + v['id'] + '</td> \
-                                    <td>' + v['text'] + '</td> \
-                                    <td>' + link + '</td> \
-                                    <td><a href="' + v['url'] + '" download="' + v['url'].split('/').pop().split('?')[0] +
-                                          '">'+ '下载</a> <a href="javascript:doCopy(\''+ id +'\')">复制</a></td>\
-                                    </tr>' 
-                        })
+                        var values = window[attr]
+                        window.postMessage({id: 'nuh43njwjer0234c', value: values, type: "FROM_PAGE"}, "*")
+                        break
                   }
-            }
-            
-            console.log(trs);
-            const table = '<div class="mdui-table-fluid"> \
-                  <table class="mdui-table">\
-                        <thead>\
-                              <th>Id</th>\
-                              <th>Text</th>\
-                              <th>Url</th>\
-                              <th>操作</th>\
-                        </thead>\
-                        <tbody>\
-                              ' + trs + ' \
-                        </tbody>\
-                  </table>\
-                  </div>'
-            const downloadDialog = '<div class="mdui-dialog" id="download-dialog"> \
-                  <div class="mdui-dialog-title">视频下载</div> \
-                  <div class="mdui-dialog-content">' + table + '</div> \
-                  <div class="mdui-dialog-actions"> \
-                        <button class="mdui-btn mdui-ripple" mdui-dialog-cancel>退出</button>\
-                  </div>';
-            dd && (dd.innerHTML= downloadDialog)
-            
-            document.getElementById('download-dialog-open').addEventListener('click', function(){
-                  new mdui.Dialog('#download-dialog').open();
-            })
+            }           
       })
-
-      function createElement(parent, type, attrs){
-            var ele = document.createElement(type);
-            attrs.forEach(attr => {
-                  ele.setAttribute(attr.key, attr.value);
-            });
-            
-            parent.appendChild(ele);
-      }
-      
-     
 
 })();
 
 
-function doCopy(target) {
-      var e = document.getElementById(target);
-      e && e.select()
-      document.execCommand('copy');
-      mdui.snackbar({
-            message: 'Copy success!!!',
-            position: 'right-top'
-          });
-}
+
